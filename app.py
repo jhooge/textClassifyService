efab9323ce558ff783b39a938d1acf0e0ff5f8ec
Jens Hooge <jhooge@SDI-MacBook.local>
2018-02-06 11:36:58 +0100

    Format prediction result in jinja html table

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import pickle
-import flask
 import keras
 import numpy as np
 import pandas as pd
-import os, sys, re, json
+import os
 
 from sklearn import metrics
 
@@ -15,6 +14,8 @@ from keras.utils import to_categorical
 from flask import Flask, request, redirect, url_for, Response
 from flask import render_template, make_response, jsonify, send_from_directory
 from werkzeug.utils import secure_filename
+
+#from jinja2 import Template
 
 UPLOAD_FOLDER = './static/data/uploads'
 ALLOWED_EXTENSIONS = set(['json'])
@@ -30,15 +31,19 @@ def load_model(fn='./static/models/cnn1d_model.h5'):
     model._make_predict_function()
     return(model)
 
+
 def load_data(path):
     data = pd.read_json(path)
     return(data)
+
 
 def load_tokenizer(fn='./static/models/tokenizer.pickle'):
     global tokenizer
     with open(fn, 'rb') as handle:
         tokenizer = pickle.load(handle)
+    tokenizer.oov_token = None ## this is a hack to handle bad serialization
     return(tokenizer)
+
 
 def prepare_docs(data, maxlen=500):
     texts = data["Abstract"]
@@ -48,39 +53,28 @@ def prepare_docs(data, maxlen=500):
     labels = to_categorical(np.asarray(labels))
     return([texts, labels])
 
-#def initialize():#
-#    model = load_my_model()
-#    texts, labels = load_data()
-#    x_test = preprocess(texts)
-#    y_test = to_categorical(np.asarray(labels))
-#
-#    return([model, x_test, y_test, labels])
-
-#@app.route("/predict")
-#def index():
-#    return(render_template("index.html"))
-
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 def uploaded_file(filename):
     return(send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename))
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET','POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
+            print('No file part')
             return redirect(request.url)
         file = request.files['file']
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
-            flash('No selected file')
+            print('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
@@ -90,7 +84,8 @@ def upload_file():
             return redirect(url_for('predict', filename=filename))
     return(render_template('index.html'))
 
-@app.route("/predict/<filename>", methods=["GET", "POST"])
+
+@app.route("/predict/<filename>", methods=["GET"])
 def predict(filename):
     data = load_data(os.path.join(app.config['UPLOAD_FOLDER'],
                              filename))
@@ -104,12 +99,18 @@ def predict(filename):
     ## add cancer probability column
     ## be aware this resets the row index !
     data_concat = pd.concat([data.reset_index(drop=True),
-                             prediction["cancer_prob"]],
-                             axis=1)
+                             prediction["cancer_prob"]], axis=1)
     data_concat = data_concat.sort_values(by="cancer_prob", ascending=False)
     data_out = data_concat[["cancer_prob", "PUI", "Title", "Source"]]
-    data_out = make_response(data_out.to_json(orient='records'))
-    return(data_out)
+#    data_out = data_out.to_json(orient='records')
+#    template = render_template("bootstrap_table.html",
+#                               data=data_out,
+#                               columns=columns,
+#                               title='Flask Bootstrap Table')
+    template = render_template('result.html', results=data_out.values.tolist())
+    return(template)
+
+
 
 if __name__ == "__main__":
     print(("* Loading Keras model and Flask starting server..."
